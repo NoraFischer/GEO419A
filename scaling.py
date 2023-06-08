384a2de4c7ccc5c6c9848fd7dd5e4f01fcf591c8
Anemone Nemorosa <nora-fischer@posteo.de>
2023-06-08 15:55:08 +0200

    Einlesen, Verarbeiten und Schreiben einer GeoTiff Datei ist nun möglich. Allerdings besteht weiterhin ein Fehler bei der Verarbeitung der Tif Datei, die uns von Marco gegeben wurde.

--- scaling.py
+++ scaling.py
@@ -1,40 +1,52 @@
 import os.path
-from osgeo import gdal
 import numpy as np
-import matplotlib.pyplot as plt
+from osgeo import gdal, osr
 
 def scale_geotiff():
 
 	path = r"C:\Users\Nora\Desktop\Dokumente\Uni\Geoinformatik\2_SoSe_2023\GEO419A_Python_FE\Test"
-	filename = r"S1A_IW_20230214T031857_DVP_RTC10_G_gpunem_A42B_VH.tif"
+	#filename = r"S1A_IW_20230214T031857_DVP_RTC10_G_gpunem_A42B_VH.tif"
 	# < Mit dieser Datei funktioniert die Umwandlung in einen Array >
-	# filename = r"test.tif"
+	filename = r"test.tif"
 
 	# set working directory
 	os.chdir(path)
 	# open geotiff raster file
-	dataset = gdal.Open(filename)
+	ds_lin = gdal.Open(filename)
 
-	# convert to array
+	# convert to numpy array
 	# < funktioniert irgendwie nicht, liegt es vielleicht an der Datei? Mit einer anderen tiff Datei funktioniert es. >
-	img_lin = dataset.GetRasterBand(1).ReadAsArray() # Fehlermeldung!
-	print(type(dataset))
-	print(type(img_lin)) # Fehler: Array ist vom Typ None
-	print(img_lin) # Fehler: Array ist None
+	arr_lin = ds_lin.GetRasterBand(1).ReadAsArray()
+	print(type(ds_lin))
+	print(type(arr_lin)) # Fehler: Array ist vom Typ None
+	print(arr_lin) # Fehler: Array ist None
 
 	# logarithmic scaling of the backscatter intensity
-	img_db = 10 * np.log10(img_lin)
+	arr_db = 10 * np.log10(arr_lin, where=arr_lin > 0) # Fehlermeldung!
+	print(arr_db)
 
-	# < write result in new file >
+	# Create new Dataset
+	driver = gdal.GetDriverByName("GTiff") # Was ist ein Driver uns was macht er?
+	driver.Register()
+	ds_db = driver.Create("new_geotiff.tif", xsize=arr_db.shape[1], ysize=arr_db.shape[0],
+						  bands=1, eType=gdal.GDT_Float32)
 
+	# set extension of output raster
+	ds_db.SetGeoTransform(ds_lin.GetGeoTransform())
+	# set spatial reference system of output raster
+	srs = osr.SpatialReference()
+	srs.ImportFromWkt(ds_lin.GetProjection())
+	ds_db.SetProjection(srs.ExportToWkt())
 
+	# Write new backscatter values to rasterband
+	band_db = ds_db.GetRasterBand(1)
+	band_db.WriteArray(arr_db)
+	band_db.SetNoDataValue(np.nan)
+	band_db.FlushCache()
 
-
-
-	# < Für die spätere Visualisierung >
-	#plt.figure()
-	#plt.imshow(array)
-
+	# close output dataset
+	band_db = None
+	ds_db = None
 
 #------------------------------------------------------------------------------
 if __name__ == '__main__':  scale_geotiff()
